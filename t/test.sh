d477e116edf7ac16dc039965e5766ec5535b0110
Tim Riemenschneider <git@tim-riemenschneider.de>
2013-02-12 21:15:07 +0100

    testcase for save-crashed-on-missing-directory
    This commit adds testcases for directories, that are deleted between index and save (and also for files, which was already fixed in tmp/pending/meta)
    
    Signed-off-by: Tim Riemenschneider <git@tim-riemenschneider.de>

--- t/test.sh
+++ t/test.sh
@@ -665,3 +665,42 @@ WVSTART "save disjoint top-level directories"
     # For now, assume that "ls -a" and "sort" use the same order.
     WVPASSEQ "$(bup ls -a src/latest)" "$(echo -e "$top_dir/\ntmp/" | sort)"
 ) || WVFAIL
+
+
+# master previously choked on this, fixed with commit 8585613c1f45f3e20feec00b24fc7e3a948fa23e
+# ("Store metadata in the index....")
+WVSTART "save-fail-missing-file"
+D=save-fail-missing-file.tmp
+export BUP_DIR="$TOP/$D/.bup"
+rm -rf $D
+mkdir $D
+bup init
+echo "content" > $D/foo
+echo "content" > $D/bar
+bup index -ux "$D"
+bup save -n save-fail-missing $D
+echo "content" > $D/baz
+bup index -ux "$D"
+rm $D/foo
+WVFAIL bup save -n save-fail-missing $D
+WVPASSEQ "$(bup ls -a save-fail-missing/latest/$TOP/$D/ | grep ^baz)" "baz"
+
+# TODO: Test for racecondition between reading a file and reading its metadata???
+
+# master still choked on this
+WVSTART "save-fail-missing-dir"
+D=save-fail-missing-dir.tmp
+export BUP_DIR="$TOP/$D/.bup"
+rm -rf $D
+mkdir $D
+bup init
+mkdir $D/foo
+mkdir $D/bar
+bup index -ux "$D"
+bup save -n save-fail-missing $D
+touch $D/bar
+mkdir $D/baz
+bup index -ux "$D"
+rmdir $D/foo
+WVFAIL bup save -n save-fail-missing $D
+WVPASSEQ "$(bup ls -a save-fail-missing/latest/$TOP/$D/ | grep ^baz/ )" "baz/"
