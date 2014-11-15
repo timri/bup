9a78c0558848e8b791804656bd368279adb45dfc
Tim Riemenschneider <git@tim-riemenschneider.de>
2014-11-15 13:15:40 +0100

    logging with timestamps
    Signed-off-by: Tim Riemenschneider <git@tim-riemenschneider.de>

--- cmd/gc-cmd.py
+++ cmd/gc-cmd.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import glob, os, stat, subprocess, sys, tempfile
+import glob, os, stat, subprocess, sys, tempfile, time
 from bup import bloom, git, midx, options, vfs
 from bup.helpers import handle_ctrl_c, log, progress, qprogress, saved_errors
 from os.path import basename
@@ -16,6 +16,7 @@ threshold   only rewrite a packfile if it's over this percent garbage [10]
 class Nonlocal:
     pass
 
+gc_start = time.time()
 
 # FIXME: unify with the walk_object() version in bup-get
 def walk_object(cat_pipe, id, verbose=None, parent_path=[],
@@ -97,16 +98,47 @@ def find_live_objects(existing_count, cat_pipe, opt):
     # FIXME: allow selection of k?
     # FIXME: support ephemeral bloom filters (i.e. *never* written to disk)
     live_objs = bloom.create(bloom_filename, expected=existing_count, k=None)
+    traversed_total = 0
+    live_count = 0
+
     for ref_name, ref_id in git.list_refs():
+        elapsed = time.time() - gc_start
+        log('gc[%02d:%02d:%02d]: traversing %s\n' % ( elapsed // 3600,
+                                                  (elapsed // 60) % 60,
+                                                   elapsed % 60,
+                                                   ref_name))
         for id, type in walk_object(cat_pipe, ref_id.encode('hex'), opt.verbose,
                                     parent_path=[ref_name],
                                     stop_at=None,
                                     include_data=None):
+            if not (traversed_total % 128):
+                elapsed = time.time() - gc_start
+                objs_per_sec = traversed_total / elapsed if elapsed else 0
+                qprogress('gc[%02d:%02d:%02d]: searching live objs %d, keep %d (%d objs/s)\r' % ( elapsed // 3600,
+                                                  (elapsed // 60) % 60,
+                                                   elapsed % 60,
+                                                   traversed_total,
+                                                   live_count,
+                                                   objs_per_sec))
             # FIXME: batch ids
+            if not live_objs.exists(id.decode('hex')):
+                live_count += 1
             live_objs.add(id.decode('hex'))
+            traversed_total += 1
+        elapsed = time.time() - gc_start
+        objs_per_sec = traversed_total / elapsed if elapsed else 0
+        progress('gc[%02d:%02d:%02d]: searched %s, traversed %d objects total (%d objs/s)\n' % ( elapsed // 3600,
+                                                  (elapsed // 60) % 60,
+                                                   elapsed % 60,
+                                                   ref_name,
+                                                   traversed_total,
+                                                   objs_per_sec))
     if opt.verbose:
-        log('gc: expecting to retain about %.2f%% unnecessary objects\n'
-            % live_objs.pfalse_positive())
+        log('gc[%02d:%02d:%02d]: expecting to retain about %.2f%% unnecessary objects\n'
+            % ( elapsed // 3600,
+               (elapsed // 60) % 60,
+                elapsed % 60,
+                live_objs.pfalse_positive()))
     return live_objs
 
 
@@ -120,7 +152,11 @@ def sweep(live_objects, existing_count, cat_pipe, opt):
             log('created ' + basename(new_pack_prefix) + '\n')
         for p in ns.stale_files:
             if opt.verbose:
-                log('gc: removing ' + basename(p) + '\n')
+                elapsed = time.time() - gc_start
+                log('gc[%02d:%02d:%02d]: removing %s\n' % ( elapsed // 3600,
+                                                           (elapsed // 60) % 60,
+                                                            elapsed % 60,
+                                                            basename(p)))
             os.unlink(p)
         ns.stale_files = []
 
@@ -133,8 +169,12 @@ def sweep(live_objects, existing_count, cat_pipe, opt):
     collect_count = 0
     for idx_name in glob.glob(os.path.join(git.repo('objects/pack'), '*.idx')):
         if opt.verbose:
-            qprogress('gc: preserving live data (%d%% complete)\r'
-                      % ((float(collect_count) / existing_count) * 100))
+            elapsed = time.time() - gc_start
+            qprogress('gc[%01d:%02d:%02d]: preserving live data (%d%% complete)\r'
+                      % ( elapsed // 3600,
+                         (elapsed // 60) % 60,
+                          elapsed % 60,
+                          ((float(collect_count) / existing_count) * 100)))
         idx = git.open_idx(idx_name)
 
         idx_live_count = 0
@@ -146,8 +186,12 @@ def sweep(live_objects, existing_count, cat_pipe, opt):
         collect_count += idx_live_count
         if idx_live_count == 0:
             if opt.verbose:
-                log('gc: %s (delete)\n'
-                    % git.repo_rel(basename(idx_name)))
+                elapsed = time.time() - gc_start
+                log('gc[%02d:%02d:%02d]: %s (delete)\n'
+                    % ( elapsed // 3600,
+                       (elapsed // 60) % 60,
+                        elapsed % 60,
+                        git.repo_rel(basename(idx_name))))
             ns.stale_files.append(idx_name)
             ns.stale_files.append(idx_name[:-3] + 'pack')
             continue
@@ -155,15 +199,30 @@ def sweep(live_objects, existing_count, cat_pipe, opt):
         live_frac = idx_live_count / float(len(idx))
         if live_frac > ((100 - opt.threshold) / 100.0):
             if opt.verbose:
-                log('gc: %s (keep: %d%% live)\n' % (git.repo_rel(basename(idx_name)),
+                elapsed = time.time() - gc_start
+                log('gc[%02d:%02d:%02d]: %s (keep: %d%% live)\n' % ( elapsed // 3600,
+                                                    (elapsed // 60) % 60,
+                                                     elapsed % 60,
+                                                     git.repo_rel(basename(idx_name)),
                                                      live_frac * 100))
             continue
 
         if opt.verbose:
-            log('gc: %s (rewrite: %.2f%% live)\n' % (basename(idx_name),
+            elapsed = time.time() - gc_start
+            log('gc[%02d:%02d:%02d]: %s (rewrite: %.2f%% live)\n' % ( elapsed // 3600,
+                                                     (elapsed // 60) % 60,
+                                                      elapsed % 60,
+                                                      basename(idx_name),
                                                       live_frac * 100))
         for i in xrange(0, len(idx)):
             sha = idx.shatable[i * 20 : (i + 1) * 20]
+            if opt.verbose and not i % 128:
+                elapsed = time.time() - gc_start
+                qprogress('gc[%02d:%02d:%02d]: this pack %.2f%% done, overall %.2f%% done\r' % ( elapsed // 3600,
+                                                     (elapsed // 60) % 60,
+                                                      elapsed % 60,
+                                                      ((float(i) / len(idx)) * 100),
+                                                      ((float(collect_count) / len(live_objects)) * 100)))
             if live_objects.exists(sha):
                 item_it = cat_pipe.get(sha.encode('hex'))
                 type = item_it.next()
@@ -173,8 +232,12 @@ def sweep(live_objects, existing_count, cat_pipe, opt):
         ns.stale_files.append(idx_name[:-3] + 'pack')
 
     if opt.verbose:
-        progress('gc: preserving live data (%d%% complete)\n'
-                 % ((float(collect_count) / existing_count) * 100))
+        elapsed = time.time() - gc_start
+        progress('gc[%02d:%02d:%02d]: preserving live data (%d%% complete)\n'
+                 % ( elapsed // 3600,
+                    (elapsed // 60) % 60,
+                     elapsed % 60,
+                    ((float(collect_count) / existing_count) * 100)))
 
     # Nothing should have recreated midx/bloom yet.
     pack_dir = git.repo('objects/pack')
@@ -187,9 +250,13 @@ def sweep(live_objects, existing_count, cat_pipe, opt):
     remove_stale_files(None)  # In case we didn't write to the writer.
 
     if opt.verbose:
-        log('gc: discarded %d%% of objects\n'
-            % ((existing_count - count_objects(pack_dir))
-               / float(existing_count) * 100))
+        elapsed = time.time() - gc_start
+        log('gc[%02d:%02d:%02d]: discarded %d%% of objects\n'
+            % ( elapsed // 3600,
+               (elapsed // 60) % 60,
+                elapsed % 60,
+              ((existing_count - count_objects(pack_dir))
+               / float(existing_count) * 100)))
 
 
 # FIXME: server mode?
@@ -217,24 +284,43 @@ git.check_repo_or_die()
 cat_pipe = vfs.cp()
 existing_count = count_objects(git.repo('objects/pack'))
 if opt.verbose:
-    log('gc: found %d objects\n' % existing_count)
+    elapsed = time.time() - gc_start
+    log('gc[%02d:%02d:%02d]: found %d objects\n' % ( elapsed // 3600,
+                                                    (elapsed // 60) % 60,
+                                                     elapsed % 60,
+                                                     existing_count))
 if not existing_count:
     if opt.verbose:
-        log('gc: nothing to collect\n')
+        elapsed = time.time() - gc_start
+        log('gc[%02d:%02d:%02d]: nothing to collect\n' % ( elapsed // 3600,
+                                                  (elapsed // 60) % 60,
+                                                   elapsed % 60))
 else:
     live_objects = find_live_objects(existing_count, cat_pipe, opt)
     try:
         # FIXME: just rename midxes and bloom, and restore them at the end if
         # we didn't change any packs?
-        if opt.verbose: log('gc: clearing midx files\n')
+        elapsed = time.time() - gc_start
+        if opt.verbose: log('gc[%02d:%02d:%02d]: clearing midx files\n' % ( elapsed // 3600,
+                                                  (elapsed // 60) % 60,
+                                                   elapsed % 60))
         midx.clear_midxes()
-        if opt.verbose: log('gc: clearing bloom filter\n')
+        elapsed = time.time() - gc_start
+        if opt.verbose: log('gc[%02d:%02d:%02d]: clearing bloom filter\n' % ( elapsed // 3600,
+                                                  (elapsed // 60) % 60,
+                                                   elapsed % 60))
         bloom.clear_bloom(git.repo('objects/pack'))
-        if opt.verbose: log('gc: clearing reflog\n')
+        elapsed = time.time() - gc_start
+        if opt.verbose: log('gc[%02d:%02d:%02d]: clearing reflog\n' % ( elapsed // 3600,
+                                                  (elapsed // 60) % 60,
+                                                   elapsed % 60))
         expirelog_cmd = ['git', 'reflog', 'expire', '--all']
         expirelog = subprocess.Popen(expirelog_cmd, preexec_fn = git._gitenv())
         git._git_wait(' '.join(expirelog_cmd), expirelog)
-        if opt.verbose: log('gc: removing unreachable data\n')
+        elapsed = time.time() - gc_start
+        if opt.verbose: log('gc[%02d:%02d:%02d]: removing unreachable data\n' % ( elapsed // 3600,
+                                                  (elapsed // 60) % 60,
+                                                   elapsed % 60))
         sweep(live_objects, existing_count, cat_pipe, opt)
     finally:
         live_objects.close()
